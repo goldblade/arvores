0f0c4ad8b46f0f1195e3d219b6b644fc49efd12e
gold <gold@hackeye>
2012-11-27 22:47:57 -0300

    add arquivos para o trabalho

---
+++ programa/arvore.pas
@@ -0,0 +1,24 @@
+{
+
+
+Trabalho final Estrutura de Dados
+
+Baseando-se na estrutura de dados em árvore “untArvore”, desenvolva as seguintes rotinas :
+
+Inserir : Conforme as regras de uma árvore binária de busca. (Elemento menor que a raiz deve ser inserido na sub-árvore esquerda; Elemento maior que a raiz deve ser inserido na sub-árvore direita).
+
+Remover : Conforme as regras de uma árvore binária de busca.
+
+
+Exibir Em nível: No exemplo da árvore A1, teríamos a seguinte exibição : a, b, c, d, f, g, h, i
+
+}
+
+
+Program arvore;
+
+uses crt, untArvore;
+
+begin
+
+end.
