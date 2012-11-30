9ee391daa288eb1bea4d59f61d96e9c2e028daed
eduardo <eduardojunior@eduardojunior.com>
2012-11-30 16:29:07 -0300

    update

--- programa/arvore.pas
+++ programa/arvore.pas
@@ -15,9 +15,67 @@ Exibir Em nível: No exemplo da árvore A1, teríamos a seguinte exibição : a,
 }
 
 
-Program arvore;
+Program arvorep;
 
 Uses crt, untArvore;
+
+
+Procedure Inserir(var Arvore:T_Arvore);
+Var
+    Elemento : T_Item;
+Begin
+    clrscr;
+    writeln('Digite o elemento');
+    readln(Elemento.Campo);
+    untArvore.Inserir(Arvore, Elemento);
+    
+    writeln;writeln('pressione uma tecla para continuar....');readkey;
+End;
+
+Procedure Deletar(var Arvore:T_Arvore);
+Begin;
+End;
+Procedure Exibir(var Arvore:T_Arvore);
+Begin
+End;
+
+Procedure Menu(var Arvore:T_Arvore);
+Var
+    opcao:integer;
+Begin
+    clrscr;
+    writeln(' ______________________________________________________________ ');
+    writeln('|                                                              |');
+    writeln('|              Trabalho Final Estrutura de Dados               |');
+    writeln('|                  Arvore de Busca Binaria                     |');
+    writeln('|            por Eduardo Junior, Mario Montino, Adriano        |');
+    writeln('|______________________________________________________________|');
+    writeln('|                                                              |');
+    writeln('|                   ESCOLHA UMA OPÇÃO                          |');
+    writeln('|                                                              |');
+    writeln('| 1: Inserir                                                   |');
+    writeln('| 2: Deletar                                                   |');
+    writeln('| 3: Exibir                                                    |');
+    writeln('| 4: Sair                                                      |');
+    writeln('|______________________________________________________________|');                                            
+    write('>> ');readln(opcao);
+    case opcao of 
+	1 : begin Inserir(Arvore); Menu(Arvore); end;
+	2 : begin Deletar(Arvore); Menu(Arvore); end;
+	3 : begin Exibir(Arvore); Menu(Arvore); end;
+	4 : begin
+	    writeln('Saindo do programa. See you later!!!');
+	    exit;
+	end;
+    else
+	clrscr;
+	textcolor(red);
+	writeln('Escolheu uma opção inválida');
+	textcolor(black);
+	delay(3000); {atraso de 3 segundos para exibicao da mensagem ao usuario e exibe o menu novamente}
+	menu(Arvore);
+    end;
+End;
 
 Var
 
@@ -26,5 +84,6 @@ Var
 Begin
 
 	IniciarArvore(Arvore);
+	Menu(Arvore);
 
 End.
--- programa/untArvore.pas
+++ programa/untArvore.pas
@@ -18,6 +18,11 @@ Type
 
 
 Procedure IniciarArvore(var Arvore : T_Arvore);
+Function chkArvoreVazia(var Arvore : T_Arvore): boolean;
+Procedure Inserir(var Arvore : T_Arvore; Elemento : T_Item);
+procedure criarRaiz(var Arvore : T_Arvore; Item : T_Item);
+procedure InserirItemEsquerda(var Pai : T_Arvore; Item : T_Item);
+procedure InserirItemDireita(var Pai : T_Arvore; Item : T_Item);
 
 
 Implementation
@@ -26,5 +31,74 @@ Procedure IniciarArvore(var Arvore : T_Arvore);
 Begin
 	Arvore := nil;
 End;
+
+Function chkArvoreVazia(var Arvore : T_Arvore) : boolean;
+Begin
+  chkArvoreVazia := Arvore = nil;
+End;
+
+Procedure Inserir(var Arvore : T_Arvore; Elemento : T_Item);
+Begin
+    {Verificar se ja existe raiz}
+    if chkArvoreVazia(Arvore) then criarRaiz(Arvore, Elemento) else begin
+	if (Elemento.Campo > Arvore^.Item.Campo) then begin
+	    writeln('o elemento é maior que o existente no no raiz criar arvore a direita ');
+	end else begin
+	    writeln('o elemento é menor criar a esquerda');
+	end; 
+    end;
+End;
+
+procedure criarRaiz(var Arvore : T_Arvore; Item : T_Item);
+var noRaiz : T_Arvore;
+begin
+  new(noRaiz);
+  noRaiz^.ESQ := nil;
+  noRaiz^.DIR := nil;
+  noRaiz^.Item := Item;
+
+  Arvore := noRaiz;
+end;
+
+procedure InserirItemEsquerda(var Pai : T_Arvore; Item : T_Item);
+var Novo : T_Arvore;
+begin
+  if (Pai <> nil) Then
+  begin
+    if (Pai^.ESQ <> nil) Then
+      writeln('Ja existe um filho a esquerda !')
+    else
+    begin
+      new(Novo);
+      Novo^.ESQ := nil;
+      Novo^.DIR := nil;
+      Novo^.Item := Item;
+
+      Pai^.ESQ := Novo;
+    end;
+  end;
+end;
+
+
+procedure InserirItemDireita(var Pai : T_Arvore; Item : T_Item);
+var Novo : T_Arvore;
+begin
+  if (Pai <> nil) Then
+  begin
+    if (Pai^.DIR <> nil) Then
+      writeln('Ja existe um filho a direita !')
+    else
+    begin
+      new(Novo);
+      Novo^.ESQ := nil;
+      Novo^.DIR := nil;
+      Novo^.Item := Item;
+
+      Pai^.DIR := Novo;
+    end;
+  end;
+end;
+
+
 
 End.
